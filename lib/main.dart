b91eb7631010c5c884d515213be861be48b1289b
PabloNAX <dasispaul@mail.ru>
2021-04-05 20:36:12 +0300

    3.6.4 -  runType method

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'my title',
+      home: MyFirstWidget(),
     );
   }
 }
@@ -98,6 +82,13 @@ class MyFirstWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    runtimeTypeMethod() {
+      print(context.runtimeType.toString());
+      return context.runtimeType;
+    }
+
+    runtimeTypeMethod();
+
     counter();
     return Container(
       child: Center(
@@ -121,6 +112,13 @@ class _MyFirstWidgetStatefullState extends State<MyFirstWidgetStatefull> {
 
   @override
   Widget build(BuildContext context) {
+    runtimeTypeMethod() {
+      print(context.runtimeType.toString());
+      return context.runtimeType;
+    }
+
+    runtimeTypeMethod();
+
     counter();
     return Container(
       child: Center(
