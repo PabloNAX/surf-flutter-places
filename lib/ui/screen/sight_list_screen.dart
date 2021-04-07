2ae978a497e8def35b3ad3edee123a35141c77c7
PabloNAX <dasispaul@mail.ru>
2021-04-07 22:50:03 +0300

    4.4.1 -  App Bar textStyle with shadow

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -30,47 +30,70 @@ class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // appBar: AppBar(
+      //   backgroundColor: Colors.transparent,
+      //   elevation: 0,
+      //   toolbarHeight: 100,
+      //   title: RichText(
+      //     maxLines: 2,
+      //     textAlign: TextAlign.left,
+      //     text: TextSpan(
+      //         text: 'С',
+      //         style: customTextStyle(color: Color(0xFF252849), shadow: true),
+      //         children: [
+      //           TextSpan(
+      //               text: 'писок ',
+      //               style:
+      //                   customTextStyle(color: Color(0xFF3B3E5B), shadow: true),
+      //               children: [
+      //                 TextSpan(
+      //                   text: '\n',
+      //                 ),
+      //                 TextSpan(
+      //                   text: 'И',
+      //                   style: customTextStyle(
+      //                       color: Color(0xFF252849), shadow: true),
+      //                 ),
+      //                 TextSpan(
+      //                   text: 'нтересных ',
+      //                   style: customTextStyle(
+      //                       color: Color(0xFF3B3E5B), shadow: true),
+      //                 ),
+      //                 TextSpan(
+      //                   text: 'М',
+      //                   style: customTextStyle(
+      //                       color: Color(0xFF252849), shadow: true),
+      //                 ),
+      //                 TextSpan(
+      //                   text: 'ест',
+      //                   style: customTextStyle(
+      //                       color: Color(0xFF3B3E5B), shadow: true),
+      //                 ),
+      //               ]),
+      //         ]),
+      //   ),
+      // ),
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
         toolbarHeight: 100,
-        title: RichText(
+        title: Text(
+          'Список \nинтересных мест',
+          style: TextStyle(
+            color: Color(0xFF252849),
+            fontWeight: FontWeight.w700,
+            fontSize: 32.0,
+            height: 1.125,
+            shadows: [
+              Shadow(
+                color: Colors.black.withOpacity(.25),
+                offset: Offset(0, 4),
+                blurRadius: 4,
+              ),
+            ],
+          ),
           maxLines: 2,
           textAlign: TextAlign.left,
-          text: TextSpan(
-              text: 'С',
-              style: customTextStyle(color: Color(0xFF252849), shadow: true),
-              children: [
-                TextSpan(
-                    text: 'писок ',
-                    style:
-                        customTextStyle(color: Color(0xFF3B3E5B), shadow: true),
-                    children: [
-                      TextSpan(
-                        text: '\n',
-                      ),
-                      TextSpan(
-                        text: 'И',
-                        style: customTextStyle(
-                            color: Color(0xFF252849), shadow: true),
-                      ),
-                      TextSpan(
-                        text: 'нтересных ',
-                        style: customTextStyle(
-                            color: Color(0xFF3B3E5B), shadow: true),
-                      ),
-                      TextSpan(
-                        text: 'М',
-                        style: customTextStyle(
-                            color: Color(0xFF252849), shadow: true),
-                      ),
-                      TextSpan(
-                        text: 'ест',
-                        style: customTextStyle(
-                            color: Color(0xFF3B3E5B), shadow: true),
-                      ),
-                    ]),
-              ]),
         ),
       ),
       body: Center(
