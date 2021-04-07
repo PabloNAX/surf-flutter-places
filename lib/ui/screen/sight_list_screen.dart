3f86077517dbf8ead534feab72a6a8cb5cc423b3
PabloNAX <dasispaul@mail.ru>
2021-04-07 22:33:48 +0300

    4.4 -  App Bar textStyle

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -6,12 +6,72 @@ class SightListScreen extends StatefulWidget {
 }
 
 class _SightListScreenState extends State<SightListScreen> {
+  // textStyle customizing function
+  TextStyle customTextStyle({
+    Color color,
+    bool shadow,
+  }) {
+    return TextStyle(
+      color: color,
+      fontWeight: FontWeight.w700,
+      fontSize: 32.0,
+      height: 1.125,
+      shadows: [
+        if (shadow)
+          Shadow(
+            color: Colors.black.withOpacity(.25),
+            offset: Offset(0, 4),
+            blurRadius: 4,
+          ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightBlueAccent,
       appBar: AppBar(
-        title: Text('title appbar'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        toolbarHeight: 100,
+        title: RichText(
+          maxLines: 2,
+          textAlign: TextAlign.left,
+          text: TextSpan(
+              text: 'С',
+              style: customTextStyle(color: Color(0xFF252849), shadow: true),
+              children: [
+                TextSpan(
+                    text: 'писок ',
+                    style:
+                        customTextStyle(color: Color(0xFF3B3E5B), shadow: true),
+                    children: [
+                      TextSpan(
+                        text: '\n',
+                      ),
+                      TextSpan(
+                        text: 'И',
+                        style: customTextStyle(
+                            color: Color(0xFF252849), shadow: true),
+                      ),
+                      TextSpan(
+                        text: 'нтересных ',
+                        style: customTextStyle(
+                            color: Color(0xFF3B3E5B), shadow: true),
+                      ),
+                      TextSpan(
+                        text: 'М',
+                        style: customTextStyle(
+                            color: Color(0xFF252849), shadow: true),
+                      ),
+                      TextSpan(
+                        text: 'ест',
+                        style: customTextStyle(
+                            color: Color(0xFF3B3E5B), shadow: true),
+                      ),
+                    ]),
+              ]),
+        ),
       ),
       body: Center(
         child: Text(
